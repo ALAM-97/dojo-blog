e63f2b75cb8c5b1bbc9d2796fdfe95db2ce9b009
ALAM-97 <alexamara97@gmail.com>
2022-01-01 15:15:28 +0100

    React Tutorial

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import BlogList from './BlogList';
+import useFetch from './useFetch';
+
+const Home = () => {
+
+    // const [name, setName] = useState('mario');
+
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id != id);
+    //     setBlogs(newBlogs);
+    // }
+
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
+
+    return ( 
+        <div className="home">
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" /*handleDelete={ handleDelete }*/ />}
+        </div>
+    );
+}
+
+export default Home;
